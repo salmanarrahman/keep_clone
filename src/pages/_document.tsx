1b9d2930fb99a3fc472ba67c5e8e770c5f9289af
salmanarrahman <salmanarrahman@gmail.com>
2023-06-21 17:42:40 +0600

    sS

---
+++ src/pages/_document.tsx
@@ -0,0 +1,17 @@
+import { Html, Main,Head, NextScript } from "next/document";
+
+export default function Document() {
+    return (
+        <Html data-theme="winter">
+            <Head>
+                <body>
+                        <Main />
+
+                        <NextScript/>
+
+
+                </body>
+            </Head>
+        </Html>
+    )
+}
